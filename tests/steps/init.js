58a3a76aedfabb60b5096b90adda5d6d0c9efcb2
Hao Chang <hao.chang@sparkol.com>
2019-05-22 12:13:56 +0100

    Update to use SSM parameters

--- tests/steps/init.js
+++ tests/steps/init.js
@@ -1,19 +1,47 @@
+const _ = require('lodash')
 const { promisify } = require('util')
 const awscred = require('awscred')
+const { REGION, STAGE } = process.env
+const AWS = require('aws-sdk')
+AWS.config.region = REGION
+const SSM = new AWS.SSM()
 
 let initialized = false
+
+const getParameters = async (keys) => {
+  const prefix = `/workshop-hao/${STAGE}/`
+  const req = {
+    Names: keys.map(key => `${prefix}${key}`)
+  }
+  const resp = await SSM.getParameters(req).promise()
+  return _.reduce(resp.Parameters, function(obj, param) {
+    obj[param.Name.substr(prefix.length)] = param.Value
+    return obj
+   }, {})
+}
 
 const init = async () => {
   if (initialized) {
     return
   }
 
-  process.env.restaurants_api      = "https://vf9e6n64u3.execute-api.us-east-1.amazonaws.com/dev/restaurants"
-  process.env.restaurants_table    = "restaurants-hao"
-  process.env.AWS_REGION           = "us-east-1"
-  process.env.cognito_user_pool_id = "us-east-1_30GkBtuZW"
-  process.env.cognito_client_id    = "54eecv61sm1grtsk7u4outguuf"
-  process.env.cognito_server_client_id = "1jgi5je4a0gfbif8ck2g9a5nj3"
+  const params = await getParameters([
+    'table_name', 
+    'cognito_user_pool_id', 
+    'cognito_web_client_id',
+    'cognito_server_client_id',
+    'url'
+  ])
+
+  console.log('SSM params loaded')
+
+  process.env.TEST_ROOT                = params.url
+  process.env.restaurants_api          = `${params.url}/restaurants`
+  process.env.restaurants_table        = params.table_name
+  process.env.AWS_REGION               = REGION
+  process.env.cognito_user_pool_id     = params.cognito_user_pool_id
+  process.env.cognito_client_id        = params.cognito_web_client_id
+  process.env.cognito_server_client_id = params.cognito_server_client_id
   
   const { credentials } = await promisify(awscred.load)()
   
@@ -21,12 +49,10 @@ const init = async () => {
   process.env.AWS_SECRET_ACCESS_KEY = credentials.secretAccessKey
 
   if (credentials.sessionToken) {
-    process.env.AWS_SESSION_TOKEN = credentials.sessionToken;
+    process.env.AWS_SESSION_TOKEN = credentials.sessionToken
   }
 
   console.log('AWS credential loaded')
-
-  process.env.TEST_ROOT = "https://vf9e6n64u3.execute-api.us-east-1.amazonaws.com/dev"
 
   initialized = true
 }
